6ac887f416aa09a1f953e2a0d574f37665a40dd1
Rabin Awal <morbid_leathal@hotmail.com>
2018-08-09 21:56:37 -0500

    Add files via upload

---
+++ com/event/eagle/controller/UserLoginController.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.event.eagle.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.event.eagle.dto.User;
+import com.event.eagle.service.UserService;
+
+@WebServlet(name = "UserLoginController", urlPatterns = { "/login" })
+public class UserLoginController extends HttpServlet {
+
+	/**
+	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+	 * methods.
+	 *
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on
+	// the + sign on the left to edit the code.">
+	/**
+	 * Handles the HTTP <code>GET</code> method.
+	 *
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+		response.sendRedirect(request.getContextPath() + "/index.jsp");
+
+	}
+
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 *
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+		String username = request.getParameter("email");
+		String password = request.getParameter("password");
+		User user = new User();
+		UserService userService = new UserService();
+		int result = userService.verifyUserLogin(username, password);
+		if (result == -1) {
+			request.setAttribute("error", "Invalid credentials");
+			request.getRequestDispatcher("/index.jsp").forward(request, response);
+		} else {
+			HttpSession session = request.getSession();
+			session.setAttribute("userid", result);
+			session.setAttribute("usertype", 0);
+
+			String name = userService.getUserName("" + result);
+			session.setAttribute("name", name);
+
+			response.sendRedirect(request.getContextPath() + "/homepage.jsp");
+		}
+	}
+
+	/**
+	 * Returns a short description of the servlet.
+	 *
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
+
+}
