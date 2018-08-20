75d4a17aab4ef0c2d741dc9dbc03d462ac69fa91
Rabin Awal <morbid_leathal@hotmail.com>
2018-08-20 00:21:28 -0500

    Create ReadEventOrganizerServlet.java
    list all available event hosts

---
+++ com/event/eagle/controller/ReadEventOrganizerServlet.java
@@ -0,0 +1,59 @@
+package com.event.eagle.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.event.eagle.dao.EventOrganizerDaoImpl;
+import com.event.eagle.dao.UserDaoImpl;
+import com.event.eagle.dto.Eventorganizer;
+import com.event.eagle.dto.User;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * Servlet implementation class ReadEventOrganizerServlet
+ */
+@WebServlet("/readeventorganizer")
+public class ReadEventOrganizerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ReadEventOrganizerServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		List<Eventorganizer> userData = new EventOrganizerDaoImpl().findAll();
+		JsonArray result = (JsonArray) new Gson().toJsonTree(userData, new TypeToken<List<Eventorganizer>>() {
+		}.getType());
+
+		response.getWriter().write(result.toString());
+	}
+
+}
