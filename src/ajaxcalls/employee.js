44fccf13952ee0c5e578e832d090b1ed0a1be237
Rabin Awal <morbid_leathal@hotmail.com>
2018-08-20 01:55:15 -0500

    Update employee.js
    fixed Admin and User roles display

--- src/ajaxcalls/employee.js
+++ src/ajaxcalls/employee.js
@@ -1,5 +1,5 @@
 /**
- * For listing employees
+ * 
  */
 
 $(document).ready(function() {
@@ -17,9 +17,9 @@ $(document).ready(function() {
 			success : function(data) {
 
 				$.each(JSON.parse(data), function(index, item) {
-					var role = "User";
+					var role = "Admin";
 					if(item.role==1){
-						role="Admin";
+						role="User";
 					}
 					$('#example2 tbody').append('<tr><td>'+item.id+'</td><td>'+item.firstname+'</td><td>'+item.lastname+'</td><td>'+item.email+'</td><td>'+role+'</td><td><button type="button" class="btn btn-danger btn-xs deleteButton" name="'+item.id+'">Delete</button></td></tr>');
 
