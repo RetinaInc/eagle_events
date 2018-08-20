f8fe1b59584aed8e278646f2a98ba84da10534f7
Rabin Awal <morbid_leathal@hotmail.com>
2018-08-20 00:48:43 -0500

    Create SeatingconstraintsDaoImpl.java

---
+++ com/event/eagle/dao/SeatingconstraintsDaoImpl.java
@@ -0,0 +1,7 @@
+package com.event.eagle.dao;
+
+import com.event.eagle.dto.Seatingconstraints;
+
+public class SeatingconstraintsDaoImpl extends GenericDaoJpaImpl<Seatingconstraints, Integer> {
+
+}
