9412a8f3ed32cf13bce2f255efc1b5fbe1254b5d
Rabin Awal <morbid_leathal@hotmail.com>
2018-08-20 00:16:25 -0500

    Create LogoutController.java
    LogoutController for Logout

---
+++ com/event/eagle/controller/LogoutController.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.event.eagle.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "LogoutController", urlPatterns = {"/logout"})
+public class LogoutController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Cache-Control", "no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+
+        if (request.getSession() == null || request.getSession().getAttribute("usertype") == null) {
+
+            response.sendRedirect(request.getContextPath() + "/index.jsp");
+
+            return;
+        }
+
+        if ((Integer) request.getSession().getAttribute("usertype") == 0) {
+
+            request.getSession().invalidate();
+
+            response.sendRedirect(request.getContextPath() + "/index.jsp");
+        } else {
+            request.getSession().invalidate();
+
+            response.sendRedirect(request.getContextPath() + "/agentlogin.jsp");
+
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
