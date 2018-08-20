92c51bb3305cd4efa8b11a8d6e14ce9f75a61bfd
Rabin Awal <morbid_leathal@hotmail.com>
2018-08-20 00:04:43 -0500

    Created employee.js
    For listing employees

---
+++ src/ajaxcalls/employee.js
@@ -0,0 +1,36 @@
+/**
+ * For listing employees
+ */
+
+$(document).ready(function() {
+
+
+		var eventid = $("#eventid").val();
+		$("div.newelement").remove();
+		$('#example2').children('tr').remove();
+
+		$.ajax({
+			url : 'reademployee',
+			type : 'POST',
+			dataType : 'html',
+			data : 'eventid=' + eventid,
+			success : function(data) {
+
+				$.each(JSON.parse(data), function(index, item) {
+					var role = "User";
+					if(item.role==1){
+						role="Admin";
+					}
+					$('#example2 tbody').append('<tr><td>'+item.id+'</td><td>'+item.firstname+'</td><td>'+item.lastname+'</td><td>'+item.email+'</td><td>'+role+'</td><td><button type="button" class="btn btn-danger btn-xs deleteButton" name="'+item.id+'">Delete</button></td></tr>');
+
+				});
+
+			},
+			error : function(e) {
+
+			}
+	});
+});
+/**
+ * 
+ */
